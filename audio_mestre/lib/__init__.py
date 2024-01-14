d16c2ad5ceed526c313dfe7631f8e549b5d7bc9f
mestrinio <ebis.pedromartins@gmail.com>
2024-01-14 17:46:07 +0000

    getting audio usage example

---
+++ audio_mestre/lib/__init__.py
@@ -0,0 +1,8 @@
+from . import keyboardActions
+from . import trackers
+from . import createPersonData
+from . import audio_pessoa_conhecida
+from . import audio_pessoa_desconhecida
+from . import computeIOU
+
+# Add here all the new scripts of the module
