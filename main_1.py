8cc1d4c349e720d6e396b812fbb0cba56d653470
mestrinio <ebis.pedromartins@gmail.com>
2024-02-02 16:00:03 +0000

    minor fix to pcd write

--- main_1.py
+++ main_1.py
@@ -331,7 +331,7 @@ def main():
         #guardar as point clouds 
         if  largura < 0.50 and comprimento < 0.50:
             if len(object_data.points) > 1500:
-                filename = f"object_pcd_{idx:03}.pcd"
+                filename = f"objects_pcd/object_pcd_{idx:03}.pcd"
                 o3d.io.write_point_cloud(filename, object_data) 
                 o3d.visualization.draw_geometries(entiti,
                                         zoom=0.3412,
