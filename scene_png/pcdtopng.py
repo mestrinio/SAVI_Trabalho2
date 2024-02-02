62e417cd2c9f0ca8e777cedc3ea446164366df2a
JoseSilva23 <josesilva8@ua.pt>
2024-02-02 13:24:07 +0000

    pcd to png

--- scene_png/pcdtopng.py
+++ scene_png/pcdtopng.py
@@ -3,24 +3,31 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Load PCD file
-pcd = o3d.io.read_point_cloud('your_point_cloud.pcd')
+paths =['object_pcd_000.pcd','object_pcd_001.pcd','object_pcd_002.pcd','object_pcd_003.pcd','object_pcd_003.pcd',
+        'object_pcd_004.pcd','object_pcd_005.pcd','object_pcd_006.pcd','object_pcd_007.pcd','object_pcd_008.pcd']
+for path in (paths):
+    pcd = o3d.io.read_point_cloud(path)
 
-# Convert Point Cloud to Numpy array
-points = np.asarray(pcd.points)
+    # Extract points and colors from the PCD file
+    points = np.asarray(pcd.points)
+    colors = np.asarray(pcd.colors)
 
-# Get the minimum and maximum coordinates to determine the image size
-min_coords = np.min(points, axis=0)
-max_coords = np.max(points, axis=0)
+    # Create an empty image
+    image = np.zeros((256, 256, 3), dtype=np.uint8)
 
-# Scale the coordinates to the range [0, 255]
-scaled_points = ((points - min_coords) / (max_coords - min_coords) * 255).astype(np.uint8)
+    # Scale the coordinates to the image size
+    min_coords = np.min(points, axis=0)
+    max_coords = np.max(points, axis=0)
 
-# Create an empty image with the same size as the scaled points
-image = np.zeros((256, 256, 3), dtype=np.uint8)
+    scaled_points = ((points - min_coords) / (max_coords - min_coords) * 255).astype(int)
 
-# Place the scaled points in the image
-image[scaled_points[:, 1], scaled_points[:, 0]] = [255, 255, 255]
+    # Place the colors in the image
+    image[scaled_points[:, 1], scaled_points[:, 0]] = (colors * 255).astype(np.uint8)
 
-# Display the image
-plt.imshow(image)
-plt.show()
+    # Display the image
+    plt.imshow(image)
+    plt.show()
+
+# Save the image as a PNG file
+# plt.imsave('output_image.png', image)
+
