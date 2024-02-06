59601edd9cb6b9452e7e82d97fc2b0067531441f
JoseSilva23 <josesilva8@ua.pt>
2024-02-06 15:34:18 +0000

    polishing files

--- deeplearning/trainer.py
+++ deeplearning/trainer.py
@@ -27,7 +27,7 @@ class Trainer():
         print(Fore.BLUE + 'Device is ' + self.device + Style.RESET_ALL)
         
         # Setup matplotlib figure
-        plt.title('Training Cats vs Dogs', fontweight="bold")
+        plt.title('Training Objects Classification', fontweight="bold")
         plt.axis([0, self.num_epochs, 0, 2])
         plt.xlabel('Epoch')
         plt.ylabel('Loss')
