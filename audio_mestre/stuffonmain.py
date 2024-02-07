7b356c0bde6dea1d74a9836ca9359b8f08e8689d
mestrinio <ebis.pedromartins@gmail.com>
2024-02-07 09:44:14 +0000

    cleanup

--- audio_mestre/stuffonmain.py
+++
@@ -1,9 +0,0 @@
-from lib.createPersonData import createPersonData
-from lib.audio_pessoa_desconhecida import play_welcome,name_prompt
-from lib.audio_pessoa_conhecida import hello_again
-from lib.computeIOU import computeIOU
-play_welcome()
-person_name = name_prompt()
-goodbye(person_name)
-hello_again(person_name)
-
